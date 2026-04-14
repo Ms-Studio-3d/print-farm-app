eece4154098eb7464eb4a51c6578843f0d2f923f
Ms-Studio-3d <mohsalah_12@yahoo.com>
2026-04-14 13:35:15 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1600,
+    height: 950,
+    minWidth: 1200,
+    minHeight: 750,
+    autoHideMenuBar: true,
+    backgroundColor: '#f1f5f9',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
