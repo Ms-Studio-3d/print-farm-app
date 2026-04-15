3b879806a4629f08a1b26e58225ca01f745fae85
Ms-Studio-3d <mohsalah_12@yahoo.com>
2026-04-15 13:06:55 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,10 +1,35 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-function safeInvoke(channel, payload = {}) {
+const CHANNELS = {
+  getDashboardData: 'db:getDashboardData',
+  getNextOrderCode: 'db:getNextOrderCode',
+  saveConfig: 'db:saveConfig',
+  savePrinter: 'db:savePrinter',
+  deletePrinter: 'db:deletePrinter',
+  saveMaterial: 'db:saveMaterial',
+  deleteMaterial: 'db:deleteMaterial',
+  createOrder: 'db:createOrder',
+  updateOrder: 'db:updateOrder',
+  deleteOrder: 'db:deleteOrder',
+  exportBackup: 'db:exportBackup',
+  importBackup: 'db:importBackup',
+  confirmDialog: 'dialog:confirm'
+};
+
+const ALLOWED_CHANNELS = new Set(Object.values(CHANNELS));
+
+function safeInvoke(channel, payload) {
+  if (!ALLOWED_CHANNELS.has(channel)) {
+    return Promise.resolve({
+      success: false,
+      message: 'IPC channel is not allowed'
+    });
+  }
+
   return ipcRenderer.invoke(channel, payload);
 }
 
-contextBridge.exposeInMainWorld('farmAPI', {
+const farmAPI = {
   appInfo: {
     name: 'Print Farm App',
     version: '1.0.0'
@@ -17,50 +42,56 @@ contextBridge.exposeInMainWorld('farmAPI', {
   },
 
   getDashboardData() {
-    return safeInvoke('db:getDashboardData');
+    return safeInvoke(CHANNELS.getDashboardData);
   },
 
   getNextOrderCode() {
-    return safeInvoke('db:getNextOrderCode');
+    return safeInvoke(CHANNELS.getNextOrderCode);
   },
 
   saveConfig(config) {
-    return safeInvoke('db:saveConfig', config);
+    return safeInvoke(CHANNELS.saveConfig, config);
   },
 
   savePrinter(printer) {
-    return safeInvoke('db:savePrinter', printer);
+    return safeInvoke(CHANNELS.savePrinter, printer);
   },
 
   deletePrinter(id) {
-    return safeInvoke('db:deletePrinter', { id });
+    return safeInvoke(CHANNELS.deletePrinter, { id });
   },
 
   saveMaterial(material) {
-    return safeInvoke('db:saveMaterial', material);
+    return safeInvoke(CHANNELS.saveMaterial, material);
   },
 
   deleteMaterial(id) {
-    return safeInvoke('db:deleteMaterial', { id });
+    return safeInvoke(CHANNELS.deleteMaterial, { id });
   },
 
   createOrder(order) {
-    return safeInvoke('db:createOrder', order);
+    return safeInvoke(CHANNELS.createOrder, order);
   },
 
   updateOrder(order) {
-    return safeInvoke('db:updateOrder', order);
+    return safeInvoke(CHANNELS.updateOrder, order);
   },
 
   deleteOrder(code) {
-    return safeInvoke('db:deleteOrder', { code });
+    return safeInvoke(CHANNELS.deleteOrder, { code });
   },
 
   exportBackup() {
-    return safeInvoke('db:exportBackup');
+    return safeInvoke(CHANNELS.exportBackup);
   },
 
   importBackup(data) {
-    return safeInvoke('db:importBackup', data);
+    return safeInvoke(CHANNELS.importBackup, data);
+  },
+
+  confirm(message) {
+    return safeInvoke(CHANNELS.confirmDialog, { message });
   }
-});
+};
+
+contextBridge.exposeInMainWorld('farmAPI', Object.freeze(farmAPI));
