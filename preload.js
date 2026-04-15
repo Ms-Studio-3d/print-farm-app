760b9c42f1f383e5193ad15eb629785c0242baf0
Ms-Studio-3d <mohsalah_12@yahoo.com>
2026-04-15 08:52:00 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,4 +1,8 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
+
+function safeInvoke(channel, payload = {}) {
+  return ipcRenderer.invoke(channel, payload);
+}
 
 contextBridge.exposeInMainWorld('farmAPI', {
   appInfo: {
@@ -10,5 +14,53 @@ contextBridge.exposeInMainWorld('farmAPI', {
 
   ping() {
     return 'pong';
+  },
+
+  getDashboardData() {
+    return safeInvoke('db:getDashboardData');
+  },
+
+  getNextOrderCode() {
+    return safeInvoke('db:getNextOrderCode');
+  },
+
+  saveConfig(config) {
+    return safeInvoke('db:saveConfig', config);
+  },
+
+  savePrinter(printer) {
+    return safeInvoke('db:savePrinter', printer);
+  },
+
+  deletePrinter(id) {
+    return safeInvoke('db:deletePrinter', { id });
+  },
+
+  saveMaterial(material) {
+    return safeInvoke('db:saveMaterial', material);
+  },
+
+  deleteMaterial(id) {
+    return safeInvoke('db:deleteMaterial', { id });
+  },
+
+  createOrder(order) {
+    return safeInvoke('db:createOrder', order);
+  },
+
+  updateOrder(order) {
+    return safeInvoke('db:updateOrder', order);
+  },
+
+  deleteOrder(code) {
+    return safeInvoke('db:deleteOrder', { code });
+  },
+
+  exportBackup() {
+    return safeInvoke('db:exportBackup');
+  },
+
+  importBackup(data) {
+    return safeInvoke('db:importBackup', data);
   }
 });
